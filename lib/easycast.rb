892607a5bfe514031ff1caf4a06518e72daa76e8
Bernard Lambeau <blambeau@gmail.com>
2018-04-17 20:03:56 +0200

    Bump to 1.3.2 and release.

--- lib/easycast.rb
+++ lib/easycast.rb
@@ -10,7 +10,7 @@ require 'rufus/scheduler'
 module Easycast
 
   # Version of Easycast software
-  VERSION = "1.3.1"
+  VERSION = "1.3.2"
 
   # Root folder of the project structure
   ROOT_FOLDER = Path.backfind('.[Gemfile]') or raise("Missing Gemfile")
