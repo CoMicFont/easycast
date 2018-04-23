652913079d3dc06053f06a4d685dea5eceada2b1
Bernard Lambeau <blambeau@gmail.com>
2018-04-23 13:55:19 +0200

    Bump to 1.4.1 and release.

--- lib/easycast.rb
+++ lib/easycast.rb
@@ -11,7 +11,7 @@ require 'digest'
 module Easycast
 
   # Version of Easycast software
-  VERSION = "1.4.0"
+  VERSION = "1.4.1"
 
   # Root folder of the project structure
   ROOT_FOLDER = Path.backfind('.[Gemfile]') or raise("Missing Gemfile")
